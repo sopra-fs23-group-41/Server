d47979b0a365a318fc1d61adfdf931b04de57096
DarleneQing <ophlia.cn@gmail.com>
2023-04-25 22:34:29 +0200

    delete unused field "points" in Player

--- src/main/java/ch/uzh/ifi/hase/soprafs23/entity/Player.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/Player.java
@@ -19,8 +19,6 @@ public class Player {
     @Column(nullable = false)
     private long gameId;
     @Column
-    private int points;
-    @Column
     private int totalScore;
     @Column
     private int roundScore;
@@ -53,10 +51,6 @@ public class Player {
         this.gameId = gameId;
     }
 
-    public void setPoints(int points) {
-        this.points = points;
-    }
-
     public void setTotalScore(int totalScore) {
         this.totalScore = totalScore;
     }
@@ -83,10 +77,6 @@ public class Player {
 
     public long getGameId() {
         return this.gameId;
-    }
-
-    public int getPoints() {
-        return this.points;
     }
 
     public int getTotalScore() {
