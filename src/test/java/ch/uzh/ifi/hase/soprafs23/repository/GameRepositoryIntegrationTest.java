3ae4ffc00065a332cafb20d801f9a35f4e000733
DarleneQing <ophlia.cn@gmail.com>
2023-05-17 15:53:45 +0200

    #113

--- src/test/java/ch/uzh/ifi/hase/soprafs23/repository/GameRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/repository/GameRepositoryIntegrationTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
-public class GameRepositoryIntegrationTest {
+class GameRepositoryIntegrationTest {
 
     @Autowired
     private TestEntityManager entityManager;
